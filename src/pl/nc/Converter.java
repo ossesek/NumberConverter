a25e2141b164b5be2a5ac334c9e52e7084d90649
Magda <madziazarczynska@gmail.com>
2021-04-04 20:19:45 +0200

    2

--- src/pl/nc/Converter.java
+++ src/pl/nc/Converter.java
@@ -1,31 +1,12 @@
 package pl.nc;
 
-public class Converter {
+public abstract class Converter {
 
-    public static String convertToRoman(int numberToConvert) {
-        String romanNumber ="";
+    protected abstract String convert(int numberToConvert);
 
-        int[] values = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
-        String[] romanLiterals = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
-
-        for(int i=0;i<values.length;i++) { //104
-            while(numberToConvert >= values[i]) {
-                numberToConvert -= values[i];
-                romanNumber += romanLiterals[i];
-            }
-        }
-
-        return romanNumber;
+    public void displayResult(int numberToConvert){
+        String result = convert(numberToConvert);
+        System.out.println("Your converted value: " + result);
     }
 
-    public static String convertToHex(int numberToConvert){
-
-        String hexNumber = Integer.toHexString(numberToConvert);
-
-        if (hexNumber.length() > 1){
-            return "0x" + hexNumber;
-        }else{
-            return "0x0" + hexNumber;
-        }
-    }
 }
--- src/pl/nc/Main.java
+++ src/pl/nc/Main.java
@@ -4,18 +4,19 @@ import java.util.Scanner;
 
 public class Main {
 
-    static String finalValue;
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int intValueToConvert = 0;
+        String conversionType = null;
+        Converter convertToHex = new ConvertToHex();
+        Converter convertToRoman = new ConvertToRoman();
 
-
-        System.out.println("Choose a number to convert: ");
-        String numberToConvert = scanner.nextLine();
+        System.out.println("Choose number and conversion type (Roman/Hexadecimal) ");
+        String userChoice = scanner.nextLine();
+        String[] parts = userChoice.split(" ");
 
         try{
-            intValueToConvert = Integer.parseInt(numberToConvert);
+            intValueToConvert = Integer.parseInt(parts[0]);
         } catch (NumberFormatException e){
             System.err.println("Input String cannot be parsed to Integer");
             System.exit(1);
@@ -24,9 +25,21 @@ public class Main {
         if(intValueToConvert <= 0){
             System.err.println("The number must be greater than zero");
             System.exit(1);
+        } else if (parts.length < 2){
+            System.err.println("Select conversion type (Roman/Hexadecimal)");
+            System.exit(1);
+        } else {
+            conversionType = parts[1].toUpperCase();
         }
 
-        finalValue = Converter.convertToRoman(intValueToConvert);
-        System.out.println("Your final value: " + finalValue);
+        if(conversionType.charAt(0) != 'R' && conversionType.charAt(0) != 'H'){
+            System.err.println("Select the correct conversion type (Roman/Hexadecimal)");
+            System.exit(1);
+        } else if (conversionType.charAt(0) == 'R') {
+            convertToRoman.displayResult(intValueToConvert);
+        } else {
+            convertToHex.displayResult(intValueToConvert);
+        }
+
     }
 }
